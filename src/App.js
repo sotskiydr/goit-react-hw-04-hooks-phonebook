c9f923a2e1d0710ab269b9d54a3437322cb4d19b
Dima Sotskiy <sotskiydr@gmail.com>
2021-12-20 00:50:19 +0200

    Phonebook is refactored

--- src/App.js
+++ src/App.js
@@ -1,68 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
 import ContactsList from './components/ContactList/ContactsList';
+import useLocalStorage from './components/hooks/uselocalstorage';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Vova Putin', number: '459-12-56' },
-      { id: 'id-2', name: 'Vitya Poroshenko', number: '443-89-12' },
-      { id: 'id-3', name: 'Vova Lenin', number: '645-17-79' },
-      { id: 'id-4', name: 'Joseph Stalin', number: '227-91-26' },
-    ],
-    filter: '',
-    name: '',
-    number: '',
-  };
+function App() {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Vova Putin', number: '459-12-56' },
+    { id: 'id-2', name: 'Vitya Poroshenko', number: '443-89-12' },
+    { id: 'id-3', name: 'Vova Lenin', number: '645-17-79' },
+    { id: 'id-4', name: 'Joseph Stalin', number: '227-91-26' },
+  ]);
 
-  formSubmitData = ({ name, number }) => {
+  const formSubmitData = ({ name, number }) => {
     const newItem = { id: nanoid(), name: name, number: number };
-    let isUnique = this.state.contacts.some(el => el.name === name);
+    let isUnique = contacts.some(el => el.name === name);
     if (!isUnique) {
-      this.setState(prevStates => ({
-        contacts: [...prevStates.contacts, newItem],
-      }));
+      setContacts(prevContacts => {
+        return [...prevContacts, newItem];
+      });
     } else {
       alert(`${name} is already in contacts`);
     }
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  renderContacts = () => {
-    const { filter, contacts } = this.state;
+  const renderContacts = () => {
     const toLowerCaseFilter = filter.toLowerCase();
     return contacts.filter(el =>
       el.name.toLowerCase().includes(toLowerCaseFilter),
     );
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(el => el.id !== id);
+    });
   };
 
-  render() {
-    const { filter } = this.state;
-    return (
-      <main className="main">
-        <h1 className="title">Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitData} />
-        <h2 className="title">Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactsList
-          renderContacts={this.renderContacts()}
-          deleteContact={this.deleteContact}
-        />
-      </main>
-    );
-  }
+  return (
+    <main className="main">
+      <h1 className="title">Phonebook</h1>
+      <ContactForm onSubmit={formSubmitData} />
+      <h2 className="title">Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactsList
+        renderContacts={renderContacts()}
+        deleteContact={deleteContact}
+      />
+    </main>
+  );
 }
 
 export default App;
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,64 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ContactForm.module.scss';
 
-class ContactForm extends React.Component {
-  state = {
-    name: '',
-    number: '',
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    if (e.currentTarget.name === 'name') setName(e.currentTarget.value);
+    if (e.currentTarget.name === 'number') setNumber(e.currentTarget.value);
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    this.props.onSubmit({
-      name: this.state.name,
-      number: this.state.number,
+    onSubmit({
+      name: name,
+      number: number,
     });
-    this.setState({ name: '', number: '' });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form className={styles['contact-form']} onSubmit={this.handleSubmit}>
-        <label className={styles.label}>
-          Name
-          <input
-            className={styles.input}
-            type="text"
-            name="name"
-            placeholder="Liubov Murarova"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label className={styles.label}>
-          Number
-          <input
-            className={styles.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            placeholder="099-999-99-99"
-            required
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-        </label>
-        <button className={styles.submit} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={styles['contact-form']} onSubmit={handleSubmit}>
+      <label className={styles.label}>
+        Name
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          placeholder="Liubov Murarova"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label className={styles.label}>
+        Number
+        <input
+          className={styles.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          placeholder="099-999-99-99"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </label>
+      <button className={styles.submit} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default ContactForm;
